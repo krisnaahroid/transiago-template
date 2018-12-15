343c3f0513a9235d370ebbc13d0341dc58642eb3
krisna ahroid <ahroidlife@gmail.com>
2018-12-15 22:51:38 +0700

    homepage

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,1 +1,10 @@
 
+$(document).ready(function () {
+
+    $('.arrow-bottom-btn').click(function() {
+    	var scrollTo = $('#transiago-intro');
+    	$('html, body').animate({
+    		scrollTop: scrollTo.offset().top
+    	}, 'slow');
+    });
+});
